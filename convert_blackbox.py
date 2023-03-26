6fc074839f58e1a062d128ea84d2b623d2b216d8
Marc Brown <marcsmobile.brown@googlemail.com>
2023-03-26 21:22:38 +0100

    Create convert_blackbox.py

---
+++ convert_blackbox.py
@@ -0,0 +1,55 @@
+import sys
+import pandas as pd
+import yaml
+
+# Get the file path from command line arguments
+file_path = sys.argv[1]
+
+# Get the output file name from command line arguments
+output_file = sys.argv[2]
+
+# Get the output directory from command line arguments
+output_dir = sys.argv[3]
+
+# Read CSV file into pandas
+df = pd.read_csv(file_path)
+
+# Filter rows based on condition
+df = df[(df['icmp'] == True) & (df['ssh-banner'] == True)]
+
+# Create an empty dictionary to store the YAML output
+yaml_output = {}
+
+# Initialize exporter_blackbox key in the YAML dictionary
+yaml_output['exporter_blackbox'] = {}
+
+# Iterate over rows in filtered dataframe
+for index, row in df.iterrows():
+    exporter_name = 'exporter_blackbox'
+    hostname = row['Hostnames']
+    ip_address = row['IP Address']
+    location = row['Location']
+    country = row['Country']
+    
+    if hostname not in yaml_output.get(exporter_name, {}):
+        yaml_output[exporter_name][hostname] = {}
+    
+    if ip_address not in yaml_output[exporter_name][hostname]:
+        yaml_output[exporter_name][hostname][ip_address] = {}
+        
+    yaml_output[exporter_name][hostname][ip_address]['location'] = location
+    yaml_output[exporter_name][hostname][ip_address]['country'] = country
+    
+    if row['icmp']:
+        yaml_output[exporter_name][hostname][ip_address]['module'] = 'icmp'
+        
+    if row['ssh-banner']:
+        yaml_output[exporter_name][hostname][f'{ip_address}:22'] = {
+            'module': 'ssh_banner',
+            'location': location,
+            'country': country
+        }
+# Write the YAML data to a file
+output_path = output_dir + output_file
+with open(output_path, 'w') as f:
+    yaml.dump(yaml_output, f)
