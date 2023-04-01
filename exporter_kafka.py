19598d68adfa55b45d473402e0962eda8209170e
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-01 16:13:28 +0100

    Update exporter_kafka.py

--- exporter_kafka.py
+++ exporter_kafka.py
@@ -16,11 +16,15 @@ def exporter_kafka(file_path, output_file, output_dir):
     yaml_output['exporter_kafka'] = {}
 
     # Iterate over rows in filtered dataframe
+    new_entries = []
     for index, row in df.iterrows():
         hostname = row['FQDN']
         ip_address = row['IP Address']
         location = row['Location']
         country = row['Country']
+
+        if ip_exists_in_yaml('exporter_kafka', ip_address, output_dir=output_dir, output_file=output_file):
+            continue
 
         if hostname not in yaml_output.get('exporter_kafka', {}):
             yaml_output['exporter_kafka'][hostname] = {}
@@ -31,7 +35,16 @@ def exporter_kafka(file_path, output_file, output_dir):
         yaml_output['exporter_kafka'][hostname]['country'] = country
         yaml_output['exporter_kafka'][hostname]['kafka_port'] = 9092
 
-    # Write the YAML data to a file
-    output_path = os.path.join(output_dir, output_file)
+        new_entries.append(row)
+
+    # Write the YAML data to a file, either appending to an existing file or creating a new file
+    if new_entries:
+        output_path = os.path.join(output_dir, output_file)
+        with open(output_path, 'a') as f:
+            yaml.dump(yaml_output, f, default_flow_style=False)
+        print("Exporter Kafka completed")
+        print(f"Total number of hosts processed: {len(new_entries)}")
+    else:
+        print("Exporter Kafka completed - nothing to do")
     with open(output_path, 'w') as f:
         yaml.dump(yaml_output, f, default_flow_style=False)
