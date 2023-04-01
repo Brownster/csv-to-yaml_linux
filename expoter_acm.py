113038af858096bec703b5920eda46f7cdcb71bd
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-01 16:40:52 +0100

    Update expoter_acm.py

--- expoter_acm.py
+++ expoter_acm.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import yaml
 
-def exporter_acm(file_path, output_file, output_dir):
+def exporter_acm(file_path, output_file, output_dir, default_listen_port=8081):
     # Read CSV file into pandas
     df = pd.read_csv(file_path)
 
@@ -22,19 +22,23 @@ def exporter_acm(file_path, output_file, output_dir):
         location = row['Location']
         country = row['Country']
         listen_port = row['App-Listen-Port']
+        ssh_username = row.get('ssh_username', 'put your username here')
+        ssh_password = row.get('ssh_password', 'put your password here')
 
         if hostname not in yaml_output.get('exporter_acm', {}):
             yaml_output['exporter_acm'][hostname] = {}
 
         yaml_output['exporter_acm'][hostname]['ip_address'] = ip_address
-        yaml_output['exporter_acm'][hostname]['listen_port'] = int(listen_port) if listen_port else None
-        yaml_output['exporter_acm'][hostname]['type'] = 'acm'
+        yaml_output['exporter_acm'][hostname]['listen_port'] = int(listen_port) if not pd.isna(listen_port) else int(default_listen_port)
+        yaml_output['exporter_acm'][hostname]['type'] = 'lsp' if 'lsp' in hostname.lower() else 'ess'
         yaml_output['exporter_acm'][hostname]['location'] = location
         yaml_output['exporter_acm'][hostname]['country'] = country
-        yaml_output['exporter_acm'][hostname]['username'] = 'put your username here'
-        yaml_output['exporter_acm'][hostname]['password'] = 'put your password here'
+        yaml_output['exporter_acm'][hostname]['username'] = ssh_username
+        yaml_output['exporter_acm'][hostname]['password'] = ssh_password
 
     # Write the YAML data to a file
     output_path = os.path.join(output_dir, output_file)
     with open(output_path, 'w') as f:
         yaml.dump(yaml_output, f, default_flow_style=False)
+        print(f"Exporter ACM completed")
+        print(f"Total number of hosts processed: {len(yaml_output['exporter_acm'])}")
