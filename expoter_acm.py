201036b11c3a90d961cb1b2725bd5975dd16b99b
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-01 16:46:28 +0100

    Update expoter_acm.py

--- expoter_acm.py
+++ expoter_acm.py
@@ -1,7 +1,3 @@
-import os
-import pandas as pd
-import yaml
-
 def exporter_acm(file_path, output_file, output_dir, default_listen_port=8081):
     # Read CSV file into pandas
     df = pd.read_csv(file_path)
@@ -16,6 +12,7 @@ def exporter_acm(file_path, output_file, output_dir, default_listen_port=8081):
     yaml_output['exporter_acm'] = {}
 
     # Iterate over rows in filtered dataframe
+    new_entries = []
     for index, row in df.iterrows():
         hostname = row['FQDN']
         ip_address = row['IP Address']
@@ -25,20 +22,30 @@ def exporter_acm(file_path, output_file, output_dir, default_listen_port=8081):
         ssh_username = row.get('ssh_username', 'put your username here')
         ssh_password = row.get('ssh_password', 'put your password here')
 
+        if ip_exists_in_yaml('exporter_acm', ip_address, output_dir=output_dir, output_file=output_file):
+            continue
+
         if hostname not in yaml_output.get('exporter_acm', {}):
             yaml_output['exporter_acm'][hostname] = {}
 
         yaml_output['exporter_acm'][hostname]['ip_address'] = ip_address
         yaml_output['exporter_acm'][hostname]['listen_port'] = int(listen_port) if not pd.isna(listen_port) else int(default_listen_port)
-        yaml_output['exporter_acm'][hostname]['type'] = 'lsp' if 'lsp' in hostname.lower() else 'ess'
+        if 'lsp' in hostname.lower():
+            yaml_output['exporter_acm'][hostname]['type'] = 'lsp'
+        elif 'ess' in hostname.lower():
+            yaml_output['exporter_acm'][hostname]['type'] = 'ess'
+        else:
+            yaml_output['exporter_acm'][hostname]['type'] = 'acm'
         yaml_output['exporter_acm'][hostname]['location'] = location
         yaml_output['exporter_acm'][hostname]['country'] = country
         yaml_output['exporter_acm'][hostname]['username'] = ssh_username
         yaml_output['exporter_acm'][hostname]['password'] = ssh_password
+
+        new_entries.append(row)
 
     # Write the YAML data to a file
     output_path = os.path.join(output_dir, output_file)
     with open(output_path, 'w') as f:
         yaml.dump(yaml_output, f, default_flow_style=False)
         print(f"Exporter ACM completed")
-        print(f"Total number of hosts processed: {len(yaml_output['exporter_acm'])}")
+        print(f"Total number of hosts processed: {len(new_entries)}")
