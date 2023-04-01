48ce3bc6427086c0ec5b1f7eab0a3bffae16bad2
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-01 17:14:37 +0100

    Update exporter_sm.py

--- exporter_sm.py
+++ exporter_sm.py
@@ -16,6 +16,7 @@ def exporter_sm(file_path, output_file, output_dir):
             raise ValueError("Invalid file type. Only CSV and Excel files are supported.")
     except Exception as e:
         print(f"Error: {e}")
+        print("Exporter SM failed")
         return
 
     # Filter rows based on condition
@@ -33,6 +34,29 @@ def exporter_sm(file_path, output_file, output_dir):
         hostname = row['Hostnames']
         ip_address = row['IP Address']
         listen_port = row['App-Listen-Port'] if not pd.isna(row['App-Listen-Port']) else listen_port_var.get()
+
+        # Check for duplicate entries
+        if ip_exists_in_yaml(exporter_name, ip_address, os.path.join(output_dir, output_file)):
+            continue
+
+        if hostname not in yaml_output[exporter_name]:
+            yaml_output[exporter_name][hostname] = {}
+        if ip_address not in yaml_output[exporter_name][hostname]:
+            yaml_output[exporter_name][hostname][ip_address] = {}
+        
+        yaml_output[exporter_name][hostname][ip_address]['listen_port'] = int(listen_port)
+        
+        new_entries.append(row)
+
+    # Write the YAML data to a file, either appending to an existing file or creating a new file
+    if new_entries:
+        with open(output_path, 'a') as f:
+            yaml.dump(yaml_output, f)
+        print("Exporter SM completed")
+        print(f"Total number of hosts processed: {len(new_entries)}")
+    else:
+        print("Exporter SM completed - nothing to do")
+
 
         # Check for duplicate entries
         if ip_exists_in_yaml(exporter_name, ip_address, os.path.join(output_dir, output_file)):
