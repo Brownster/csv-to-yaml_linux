41e8915ce8072370e369472874c4401f6cb5102f
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-01 16:27:12 +0100

    Update exporter_drac.py

--- exporter_drac.py
+++ exporter_drac.py
@@ -7,7 +7,10 @@ def exporter_drac(file_path, output_file, output_dir):
     df = pd.read_csv(file_path)
 
     # Filter rows based on condition
-    df = df[df['Exporter_name_app'] == 'exporter_drac']
+    df_filtered = df[df['Exporter_name_app'] == 'exporter_drac']
+
+    # Define output path
+    output_path = os.path.join(output_dir, output_file)
 
     # Create an empty dictionary to store the YAML output
     yaml_output = {}
@@ -16,23 +19,34 @@ def exporter_drac(file_path, output_file, output_dir):
     yaml_output['exporter_drac'] = {}
 
     # Iterate over rows in filtered dataframe
-    for index, row in df.iterrows():
+    new_entries = []
+    for index, row in df_filtered.iterrows():
         hostname = row['FQDN']
         ip_address = row['IP Address']
         location = row['Location']
         country = row['Country']
 
-        if hostname not in yaml_output.get('exporter_drac', {}):
+        if ip_exists_in_yaml('exporter_drac', ip_address, output_path=output_path):
+            continue
+
+        if hostname not in yaml_output['exporter_drac']:
             yaml_output['exporter_drac'][hostname] = {}
 
         yaml_output['exporter_drac'][hostname]['ip_address'] = ip_address
-        yaml_output['exporter_drac'][hostname]['listen_port'] = int(row['App-Listen-Port'])
+        yaml_output['exporter_drac'][hostname]['listen_port'] = 623
         yaml_output['exporter_drac'][hostname]['location'] = location
         yaml_output['exporter_drac'][hostname]['country'] = country
         yaml_output['exporter_drac'][hostname]['snmp_version'] = 2
-        yaml_output['exporter_drac'][hostname]['community'] = 'ENC'
+        yaml_output['exporter_drac'][hostname]['community'] = row.get('comm_string', 'ENC')
 
-    # Write the YAML data to a file
-    output_path = os.path.join(output_dir, output_file)
-    with open(output_path, 'w') as f:
-        yaml.dump(yaml_output, f, default_flow_style=False)
+        new_entries.append(row)
+
+    # Write the YAML data to a file, either appending to an existing file or creating a new file
+    if new_entries:
+        with open(output_path, 'a') as f:
+            yaml.dump(yaml_output, f, default_flow_style=False)
+        print("Exporter DRAC completed")
+        print(f"Total number of hosts processed: {len(new_entries)}")
+    else:
+        print("Exporter DRAC completed - nothing to do")
+
